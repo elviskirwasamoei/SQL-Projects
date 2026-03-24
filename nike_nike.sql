5814ba9f425e43c394be0d78bf69570ca363b613
elviskirwasamoei <125877877+elviskirwasamoei@users.noreply.github.com>
2026-03-24 10:02:41 +0300

    Update nike_nike.sql

--- nike_nike.sql
+++ nike_nike.sql
@@ -5,3 +5,11 @@ Nike Sales Data Analysis (SQL Project)
 
   Overview
          This project analyzes Nike sales data using SQL to uncover insights on revenue, product performance, regional trends, and profitability. The dataset was cleaned, transformed, and analyzed to support data-driven business decisions.
+
+  Objectives
+        Analyze sales performance by region
+        Identify top-performing products
+        Evaluate profitability and margins
+        Detect seasonal trends
+
+      
